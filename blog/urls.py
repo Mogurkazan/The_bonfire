b9dab0e409c986731228c826df75a1eebc379fae
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2024-08-04 10:38:06 +0200

    add post titles

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/comment/', views.add_comment, name='add_comment'),
     path('post/<int:pk>/comments/', views.load_comments, name='load_comments'),
+    path('post/new/', views.post_create, name='post_create'),  # Añadir esta línea
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', next_page='private_area'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
